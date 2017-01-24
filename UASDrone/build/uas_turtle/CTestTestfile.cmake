19761f5fa912b32b4f39748923ae2a4aecaea5a4
Tyler Thome <tyler_thome@yahoo.com>
2017-01-24 12:56:16 -0800

    add a .gitignore, add clean script

--- UASDrone/build/uas_turtle/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/tyler/ground-system/UASDrone/src/uas_turtle
-# Build directory: /home/tyler/ground-system/UASDrone/build/uas_turtle
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
